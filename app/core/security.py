321c50ebd9a6357f7369f1eb0469642332686ba5
Yagyesh Vijay <yagyeshvijay017@gmail.com>
2026-09-15 07:02:03 +0000

    Implement JWT login authentication

--- app/core/security.py
+++ app/core/security.py
@@ -1,16 +1,41 @@
 from passlib.context import CryptContext
-pwd_context=CryptContext(
-    schemes=["bcrypt"], 
+from jose import jwt
+from datetime import datetime, timedelta
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
     deprecated="auto"
 )
+
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 def verify_password(
-        plain_password: str,
-        hashed_password:str
-    )-> bool:
+    plain_password: str,
+    hashed_password: str
+) -> bool:
     return pwd_context.verify(
         plain_password,
         hashed_password
     )
+
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({
+        "exp": expire
+    })
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+    return encoded_jwt
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,17 +1,19 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database.database import get_db
 from app.database.models import User
-from app.schemas.auth import UserCreate
-from app.core.security import hash_password
-
+from app.schemas.auth import UserCreate, UserLogin
+from app.core.security import (
+    hash_password,
+    verify_password,
+    create_access_token
+)
 
 router = APIRouter(
     prefix="/api/v1/auth",
     tags=["Authentication"]
 )
-
 
 @router.post("/register")
 def register_user(
@@ -44,4 +46,41 @@ def register_user(
         "message": "User registered successfully",
         "username": new_user.username,
         "user_id": new_user.id
+    }
+
+@router.post("/login")
+def login_user(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+    existing_user = (
+        db.query(User)
+        .filter(User.username == user.username)
+        .first()
+    )
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password"
+        )
+
+    password_correct = verify_password(
+        user.password,
+        existing_user.hashed_password
+    )
+
+    if not password_correct:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password"
+        )
+
+    access_token = create_access_token({
+        "sub": existing_user.username
+    })
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
     }
